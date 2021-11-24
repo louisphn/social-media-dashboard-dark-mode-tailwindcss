30d379b17a79a15b7a3b1b2627b984b828fa5dc1
Louis Phan <louisphan@notreallouisss.local>
2021-11-25 08:44:37 +0900

    set default theme to system theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from "next-themes";
 
-import 'tailwindcss/tailwind.css';
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" enableSystem defaultTheme="system">
       <Component {...pageProps} />
     </ThemeProvider>
   );
